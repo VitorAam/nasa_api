ecae60fee6023d902ef2393e6ab978d7f9a593ab
VITOR AUGUSTO ALMEIDA DE MOURA <v.augusto.am@gmail.com>
2022-05-12 21:11:14 -0300

    att

--- main_page/index.js
+++ main_page/index.js
@@ -2,22 +2,25 @@ $('#btn').click(function(event){
     event.preventDefault();
     let selectedDate = $('#date').val();
     const serverKey = 'aT1LRxrFFc2ggV3gXlcENwmFDfTq3ueFbmMXl9pa&date='
-    $.ajax({
+    try{
+       $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=${serverKey}${selectedDate}`,
         success: function (result){
-            console.log(this.url)
             showToUser(result)
         }
-    })
+    }) 
+    } catch (erro) {
+        console.log(erro)
+    }
+    
 })
 
 function showToUser(result){
     if(result.media_type == 'image'){
         $('#video_image').html(`<img id="image" src="${result.url}" alt="${result.title}">`)
     } else {
-        $('#video_image').html(`<video>
-        <source src='${result.url}'></video>`)
+        $('#video_image').html(`<iframe src="${result.url}" id="video"></iframe>`)
     }
     
-    $('#explanation').html(`<strong>Texto de explicação da imagem (em inglês):</strong> ${result.explanation}`)
+    $('#explanation').html(`<strong>Text:</strong> ${result.explanation}`)
 }
